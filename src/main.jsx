da8521b8001894664eef2a3a182fe3d99a2c9252
iraitz-vikings <iraitz.araolaza@gmail.com>
2026-07-31 23:46:05 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Admin from "./Admin.jsx";
+import "./styles.css";
+
+const isAdmin = window.location.pathname.startsWith("/admin");
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+      {isAdmin ? <Admin /> : <App />}
+        </React.StrictMode>
+        );
+        
