805e428866bb7ff43929aa962b26795a5d374262
iraitz-vikings <iraitz.araolaza@gmail.com>
2026-08-09 14:45:58 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import Admin from "./Admin.jsx";
 import Galeria from "./Galeria.jsx";
 import TorneoPage from "./TorneoPage.jsx";
 import Historico from "./Historico.jsx";
+import Socios from "./Socios.jsx";
 import { LanguageProvider } from "./i18n.jsx";
 import "./styles.css";
 
@@ -12,20 +13,22 @@ const path = window.location.pathname;
 const isAdmin = path.startsWith("/admin");
 const isGaleria = path.startsWith("/galeria");
 const isHistorico = path.startsWith("/historico");
+const isSocios = path.startsWith("/socios");
 const matchTorneo = path.match(/^\/torneo\/([^/]+)/);
 
 function Pagina() {
   if (isAdmin) return <Admin />;
   if (isGaleria) return <Galeria />;
   if (isHistorico) return <Historico />;
+  if (isSocios) return <Socios />;
   if (matchTorneo) return <TorneoPage id={matchTorneo[1]} />;
   return <App />;
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <LanguageProvider>
-        <Pagina />
-      </LanguageProvider>
-        </React.StrictMode>
-        );
+    <LanguageProvider>
+      <Pagina />
+    </LanguageProvider>
+  </React.StrictMode>
+);
