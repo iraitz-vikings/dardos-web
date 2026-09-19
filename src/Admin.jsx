cef3d7e074854dd2d67632cd83d990b692336275
IraitzBitek <iraitzbitek@gmail.com>
2026-09-19 13:35:39 +0200

    app

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,5 +1,5 @@
   import { useEffect, useState } from "react";
-const ADMIN_EMBLEM_URL = "https://res.cloudinary.com/lodi1y1k/image/upload/v1786283841/vikings-logo-transparente_bjtv7c.png";
+const ADMIN_EMBLEM_URL = "https://res.cloudinary.com/lodi1y1k/image/upload/c_fill,g_auto,h_192,w_192/f_png/v1789816550/IMG-20260910-WA0004_o3n7ly.png";
 import AdminTorneosClub from "./AdminTorneosClub.jsx";
 import AdminPatrocinadores from "./AdminPatrocinadores.jsx";
 import AdminSocios from "./AdminSocios.jsx";
